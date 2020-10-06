6e95eefee9234ab4dacc6f5c516fa9f915e7f5e8
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-10-06 16:20:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_coocoo_flappy_flaps = data.each_with_object({}) do |(key, value), hash |
+    value.each do |inner_key, inner_value|
+      inner_value.each do |name|
+        if !hash[name]
+          hash[name] = {}  
+        end
+        if !hash[name][key]
+          hash[name][key] = [] 
+        end
+        hash[name][key] << inner_key.to_s
+      end
+    end
+    organized_coocoo_flappy_flaps 
+  end
 end
