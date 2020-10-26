e85eb308f3c87207c7e053ed2937759695efa8a3
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-26 18:40:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = Hash.new { |h, k| h[k] = {} }
+  data.each do |key, value|
+    value.each do |key1, value1|
+      value1.each do |bird|
+        if !new_hash[bird]
+          new_hash[bird][key] = [key1.to_s]
+        elsif new_hash[bird] && !new_hash[bird][key]
+          new_hash[bird][key] = [key1.to_s]
+        else  
+          new_hash[bird][key] << key1.to_s
+        end
+      end
+
+    end
+  end
+  new_hash
 end
